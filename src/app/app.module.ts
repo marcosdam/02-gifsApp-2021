00f2aa38e61c10e76e22856593d9d168691881a2
Marcos <marcos.ledesma.picazo@cieep.com>
2021-03-15 14:20:56 +0100

    localStorage y búsqueda terminados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
 import { GifsService } from './gifs/services/gifs.service';
+// Components
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +19,8 @@ import { GifsService } from './gifs/services/gifs.service';
   imports: [
     BrowserModule,
     SharedModule,
-    GifsModule
+    GifsModule,
+    HttpClientModule
   ],
   providers: [
     GifsService
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,28 +1,54 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey: string = 'sWp5BXp5KLIcquaqlV9qz31IBwc2Q3om';
+
   private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
 
   get historial() {
     return this._historial;
   }
 
-  buscarGifs( query: string = '' ){
-
-    query = query.trim().toLowerCase();
-
-    if ( !this._historial.includes( query ) ){
-      this._historial.unshift( query );
+  constructor( private http: HttpClient ) {
+    // Recuperar historial del localStorage al inicar servicio
+    if (localStorage.getItem('historial')){
+      this._historial = JSON.parse( localStorage.getItem('historial')! ) || [];  // Si devuelve nulo, devuélveme un [] vacío
+      this.resultados = JSON.parse( localStorage.getItem('resultados')! ) || [];
     }
-
-    this._historial = this._historial.splice(0,10);
-
-    console.log( this._historial );
   }
 
-  constructor() { }
+  buscarGifs( query: string = '' ){
+    query = query.trim().toLowerCase();
+    if ( !this._historial.includes( query ) ){
+      this._historial.unshift( query );
+      this._historial = this._historial.splice(0,10);
+    }
+
+    // Peticiones Http Angular (retornan Observables -> puedo concatenar, disparar otra petición simultáneamente, etc)
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${ this.apiKey }&q=${ query }&limit=10`)
+      .subscribe( (resp) => {
+        console.log( resp.data );
+        this.resultados = resp.data;
+
+        localStorage.setItem( 'resultados', JSON.stringify( this.resultados ) );
+      });
+
+    /* Fetch Javascript (peticiones Http -> Menos funcionalidades que Angular (rxjs))
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=sWp5BXp5KLIcquaqlV9qz31IBwc2Q3om&q=meme')
+      .then( resp => {
+        resp.json().then( datos => {console.log(datos);
+        })
+      })
+    */
+  }
+
+
 }
